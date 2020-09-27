f2957029fe2eb9a22faa55683af33c4495368a4f
Sarthak Gupta <sg191107@gmail.com>
2020-09-27 16:21:18 +0530

    Home and  edit fragments added

---
+++ Notes-App/app/src/main/java/com/sarthaksavasil/notes_app/HomeFragment.kt
@@ -0,0 +1,32 @@
+package com.sarthaksavasil.notes_app
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.Navigation
+import kotlinx.android.synthetic.main.fragment_home.*
+
+class HomeFragment : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_home, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        button_add.setOnClickListener {
+            val action = HomeFragmentDirections.actionToedit()
+            Navigation.findNavController(it).navigate(action)
+        }
+
+    }
+
+
+}
--- Notes-App/app/src/main/java/com/sarthaksavasil/notes_app/MainActivity.kt
+++ Notes-App/app/src/main/java/com/sarthaksavasil/notes_app/MainActivity.kt
@@ -2,10 +2,24 @@ package com.sarthaksavasil.notes_app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.ui.NavigationUI
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val navController = Navigation.findNavController(this,R.id.fragment)
+        NavigationUI.setupActionBarWithNavController(this,navController)
+
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return NavigationUI.navigateUp(
+            Navigation.findNavController(this,R.id.fragment),
+            null
+        )
     }
 }
